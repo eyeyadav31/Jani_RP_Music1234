89d67962370717a1c41c7f4df8a1959e822e0a52
eyeyadav31 <eyeyadav31@gmail.com>
2025-05-28 19:18:16 +0530

    Update play.py

--- EsproMusic/utils/inline/play.py
+++ EsproMusic/utils/inline/play.py
@@ -85,6 +85,12 @@ def stream_markup(_, chat_id):
            # InlineKeyboardButton(text="⎋", callback_data=f"ADMIN Replay|{chat_id}"),
          #   InlineKeyboardButton(text="➘", callback_data=f"ADMIN Stop|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+
+            [
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
+        ],
+            
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
