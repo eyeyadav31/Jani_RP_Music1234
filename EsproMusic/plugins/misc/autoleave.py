1fed052ffb932cb42a4aa3622a460e3a84c5d636
ANURAG X MUSIC 💞 <lokendrhopal@gmail.com>
2025-06-10 16:45:08 +0530

    Update autoleave.py

--- EsproMusic/plugins/misc/autoleave.py
+++ EsproMusic/plugins/misc/autoleave.py
@@ -26,8 +26,8 @@ async def auto_leave():
                         ]:
                             if (
                                 i.chat.id != config.LOGGER_ID
-                                and i.chat.id != -1001686672798
-                                and i.chat.id != -1001549206010
+                                and i.chat.id != -1002664337527
+                                and i.chat.id != -1002666396577
                             ):
                                 if left == 20:
                                     continue
