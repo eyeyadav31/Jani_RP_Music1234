047feb333611ed14bdd6c8b2596e697d38cca6dc
ANURAG X MUSIC 💞 <lokendrhopal@gmail.com>
2025-06-18 11:59:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ANURAG-KING-MAX/ANURAGPROMUSIC",
+    "https://github.com/ANURAG-KING-MAX/MUSIC_REPO.01",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
